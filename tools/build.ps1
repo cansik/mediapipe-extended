4c061f74f87344f95e8c89368c1e70b26ca7a9c5
Florian Bruggisser <github@broox.ch>
2023-04-26 12:09:05 +0200

    added bazel clean commad to build script to avoid reusing wrong python builds

--- tools/build.ps1
+++ tools/build.ps1
@@ -8,7 +8,8 @@ param(
     [string]$MediaPipeRepository = "https://github.com/cansik/mediapipe.git",
     [string]$MediaPipeBranch = "face-geometry-python",
     [switch]$SkipRepositorySetup = $False,
-    [switch]$OnlyInstallDependencies = $False
+    [switch]$OnlyInstallDependencies = $False,
+    [switch]$Clean = $False
 )
 
 function Replace-In-File([string]$InputFile, $Tokens)
@@ -28,14 +29,16 @@ function Try-Resolve-Path([string]$Path)
     return $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
 }
 
-function Create-Dir-If-Needed([string]$Path) {
+function Create-Dir-If-Needed([string]$Path)
+{
     if (!(Test-Path $Path))
     {
         New-Item -Path $Path -ItemType Directory
     }
 }
 
-function Find-File-Or-Exit([string]$Path, [string]$ErrorMessage = "") {
+function Find-File-Or-Exit([string]$Path, [string]$ErrorMessage = "")
+{
     [array]$files = Get-ChildItem $Path
     if ($files.Length -eq 0)
     {
@@ -56,6 +59,7 @@ $IsMacOSArm64 = $IsMacOS -And ($( arch ) -eq "arm64")
 $LinkOpenCV = $False
 
 Write-Host -ForegroundColor Blue "Mediapipe Build Script"
+Write-Host -ForegroundColor Blue "Python version: $(python --version)"
 Write-Host -ForegroundColor Blue "building $PackageName in $BuildPath..."
 
 # install os specific pre-requisites and set path
@@ -69,6 +73,11 @@ if (-Not$SkipRepositorySetup)
 {
     if (Test-Path $BuildPath)
     {
+        Push-Location $BuildPath
+        Write-Host -ForegroundColor Yellow "Cleaning bazel..."
+        bazel clean
+        Pop-Location
+
         Remove-Item -Recurse -Force -Path $BuildPath
     }
 }
@@ -90,7 +99,9 @@ if ($IsMacOS)
     if ($IsMacOSArm64)
     {
         $BDistPlatformName = "macosx-12_0-arm64"
-    } else {
+    }
+    else
+    {
         $BDistPlatformName = "macosx_12_0_x86_64"
     }
 
@@ -104,7 +115,7 @@ elseif ($IsWindows)
 {
     # choco install -y --force bazel --version=5.1.0
     choco install -y bazelisk protoc
-    choco install opencv --version=3.4.10 --params="/InstallationPath:$LibPath"
+    choco install opencv --version = 3.4.10 --params = "/InstallationPath:$LibPath"
 
     # extend path with zip folder name
     $WinOpenCVBuildPath = Join-Path $LibPath "opencv/build"
@@ -127,7 +138,8 @@ elseif ($IsLinux)
     pip install auditwheel
 }
 
-if ($OnlyInstallDependencies) {
+if ($OnlyInstallDependencies)
+{
     Write-Host -ForegroundColor Blue "Installed dependencies only. Now exiting build script!"
     exit 0
 }
@@ -140,6 +152,12 @@ if (-Not$SkipRepositorySetup)
 }
 
 Push-Location $BuildPath
+
+if ($Clean)
+{
+    Write-Host -ForegroundColor Yellow "Cleaning bazel..."
+    bazel clean
+}
 
 # install pre-requisites
 pip install wheel
